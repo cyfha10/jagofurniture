ef491a37c2b8fb7b886e0a9548695ec18f02d89e
Frankky <fireblast.fire@gmail.com>
2025-09-07 23:24:01 +0700

    Update Routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,5 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['default_controller'] = 'home';
 $route['product'] = 'home/product';
+$route['aboutus'] = 'home/about';
+$route['blog'] = 'home/blog';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
