85155be09a5f3be4c90150d6c411277aacae4414
Frankky <fireblast.fire@gmail.com>
2025-09-07 23:43:38 +0700

    Update About

---
+++ application/modules/home/views/page/about.php
@@ -0,0 +1,77 @@
+<main id="content">
+      <section class="d-flex flex-column bg-img-cover-center vh-100 pt-xxl-13 custom-height-sm"
+         style="background-image: url('assets/images/bg-about-us-02.jpg')">
+        <div class="d-flex flex-column h-100 justify-content-center">
+          <div class="container container-xxl">
+            <h1 class="fs-60 mb-4">About Us</h1>
+            <p class="mb-0 mxw-435px">"Jago Furniture"<br>
+Furnitur custom yang dirancang untuk menciptakan ruangan yang lebih hidup dan fungsional untuk bisnis dan rumah Anda.</p>
+          </div>
+        </div>
+      </section>
+      <section class="pt-11 pt-lg-15">
+        <div class="container">
+          <!-- <div class="mxw-924 mx-auto mb-7">
+            <h2 class="fs-30 fs-md-40 lh-15 text-center mb-0">Jago Furniture hadir untuk membantu Anda menciptakan furnitur yang benar-benar mencerminkan kepribadian dan kebutuhan ruang Anda.</h2>
+          </div> -->
+          <div class="mxw-830 mx-auto mb-8">
+            <p class="fs-20 text-primary font-weight-500 mb-5">Jago Furniture hadir untuk membantu Anda menciptakan furnitur yang benar-benar mencerminkan kepribadian dan kebutuhan ruang Anda.</p>
+            <p class="mb-1">Di Jago Furniture, kami percaya bahwa setiap rumah memiliki cerita, dan furnitur adalah bagian penting dari cerita itu.
+Itulah mengapa kami menawarkan layanan custom furniture – bukan sekadar menjual produk, tetapi membantu Anda mewujudkan ide menjadi kenyataan.<br><br>
+
+Dengan tim desainer berpengalaman dan pengrajin profesional, kami siap menciptakan furnitur yang unik, fungsional, dan berkualitas tinggi.
+Mulai dari pemilihan material, desain, hingga finishing, setiap detail dikerjakan dengan teliti agar sesuai dengan gaya hidup, ukuran ruangan, dan selera pribadi Anda.<br><br>
+
+Kami berkomitmen menghadirkan pengalaman yang menyenangkan:
+
+Konsultasi desain untuk menemukan konsep terbaik
+
+Proses produksi transparan sehingga Anda tahu setiap tahap pembuatan
+
+Hasil akhir berkualitas premium yang bertahan lama
+
+Bersama Jago Furniture, rumah Anda tidak hanya nyaman, tetapi juga penuh karakter.</p>
+          </div>
+          <img src="assets/images/b-17.jpg" alt="Image" class="mb-10">
+          <div class="mxw-830 mx-auto">
+            
+            <p class="mb-0">Kami bekerja dengan proses yang jelas dan kolaboratif:
+
+Konsultasi awal – mendengar ide, gaya, dan kebutuhan Anda.
+
+Desain 3D & revisi – supaya Anda bisa melihat wujud furnitur sebelum diproduksi.
+
+Produksi oleh pengrajin ahli – dengan standar kualitas tinggi dan ketelitian pada detail.
+
+Pengiriman & pemasangan – hingga furnitur siap mempercantik ruang Anda.<br><br>
+
+Lebih dari sekadar membuat meja, lemari, atau kursi, misi kami adalah mewujudkan ruang yang nyaman, fungsional, dan mencerminkan siapa Anda.
+Dengan Jago Furniture, Anda mendapatkan furnitur yang bukan hanya dipakai – tetapi juga dinikmati setiap hari.</p>
+          </div>
+        </div>
+      </section>
+      
+      
+      <section class="py-11 py-lg-15">
+        <div class="container">
+          <div class="row no-gutters">
+            <div class="col-lg-3 mb-8 mb-lg-0">
+              <h2 class="fs-30 fs-md-40 lh-15 mb-6 pt-lg-5">Hubungi Kami</h2>
+              <p class="font-weight-bold text-primary mb-4">Alamat Bisnis</p>
+              <address class="mb-6">
+                Jkt Outer Ring Road Jl. Raya Pondok Randu No.2, RW.2, Duri Kosambi, Kecamatan Cengkareng, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11750
+              </address>
+              <p class="font-weight-bold text-primary mb-3">Phone / WA</p>
+              <p class="mb-0">+62 877‑7630‑4443</p>
+              <!-- <p class="mb-7">hello@domain.com</p> -->
+              <a href="https://wa.me/6287776304443" target="_blank" class="btn btn-primary">contact us</a>
+            </div>
+            <div class="col-lg-9">
+              <div class="pl-lg-10">
+                <img src="assets/images/b-21.jpg" alt="Image">
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
