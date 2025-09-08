0ff0f8bc2a6bc8db226002bc213077011b46bdce
Frankky <fireblast.fire@gmail.com>
2025-09-08 22:38:46 +0700

    Update Routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -5,5 +5,7 @@ $route['default_controller'] = 'home';
 $route['product'] = 'home/product';
 $route['aboutus'] = 'home/about';
 $route['blog'] = 'home/blog';
+$route['blog/(:any)'] = 'blog/detail/$1'; 
+$route['categories/(:any)'] = 'home/categories/$1'; 
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
