1614552380d8f6f5580d81c4ac7d0491b825816b
Frankky <fireblast.fire@gmail.com>
2025-09-14 11:52:24 +0700

    Update Sidebar

--- application/modules/templates/views/sidebar.php
+++ application/modules/templates/views/sidebar.php
@@ -4,6 +4,7 @@
         <!-- sidebar menu start-->
         <ul class="sidebar-menu" id="nav-accordion">
             
+            <li><a href="<?= base_url('header'); ?>"><i class="fa fa-envelope"></i>HEADER</a></li>
             <li><a href="<?= base_url('about'); ?>"><i class="fa fa-user-circle"></i>ABOUT US</a></li>
             <li><a href="<?= base_url('banner'); ?>"><i class="fa fa-briefcase"></i> BANNER</a></li>
             <li><a href="<?= base_url('blogs'); ?>"><i class="fa fa-tag"></i>BLOG</a></li>
@@ -11,7 +12,6 @@
             <li><a href="<?= base_url('products'); ?>"><i class="fa fa-image"></i>PRODUCT</a></li>
             <li><a href="<?= base_url('testimoni'); ?>"><i class="fa fa-file-text"></i>TESTIMONI</a></li>
             <li><a href="<?= base_url('socialmedia'); ?>"><i class="fa fa-instagram"></i>SOCIAL MEDIA</a></li>
-            <li><a href="<?= base_url('header'); ?>"><i class="fa fa-envelope"></i>HEADER</a></li>
         </ul>
         <!-- sidebar menu end-->
     </div>
